ef84ffc0d441bc1d653463c7aa6a86e919bd165c
Niccolò Albanese <nick_albanese@virgilio.it>
2024-12-13 16:58:20 +0100

    90%

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -7,25 +7,23 @@ function MyNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        <Navbar.Brand href="#home">
+          <svg
+            height="20"
+            viewBox="0 0 83 20"
+            width="83"
+            xmlns="http://www.w3.org/2000/svg"
+            className="logo"
+            aria-hidden="true"
+          ></svg>
+        </Navbar.Brand>
       </Container>
     </Navbar>
   )
